20a1f17b93a7cb067c48fe78c94c487959a07935
w3bn00b <32405548+w3bn00b@users.noreply.github.com>
2017-10-02 00:20:33 +0900

    added new feature, daily schedule

--- src/tCafeteria.py
+++ src/tCafeteria.py
@@ -2,9 +2,9 @@
 
 # Copyright (c) 2017 w3bn00b
 # See the file LICENSE for copying permission.
-# tanbang-cafeteria v1.1
+# tanbang-cafeteria v1.2
 # author : w3bn00b
-# description : 대전 학교의 급식 정보를 파싱해옵니다
+# description : 중학교의 급식정보와 학사일정 파싱해옵니다
 # Usage : cafe = tCafeteria("학교코드", "관할지역 코드")
 # Required library : datetime, bs4, requests
 
@@ -15,6 +15,8 @@ import requests
 
 
 class tCafeteria:
+	locale = 'DAEJEON'
+	
 	region = {
 		'SEOUL':'stu.sen.go.kr',
 		'INCHEON':'stu.ice.go.kr',
@@ -35,14 +37,18 @@ class tCafeteria:
 		'JEJU':'stu.jje.go.kr'
 	}
 	
-	locale = 'DAEJEON'
-	
 	def __init__(self, schoolcode, locale):
 		self.schoolcode = schoolcode #학교코드 설정
 		self.locale = locale
 	
 	def getDate(self):
-		return datetime.today().day	#오늘 날짜
+	#	return datetime.today().day	#오늘 날짜
+		return 12
+	def getMonth(self):
+	#	return datetime.today().month #달
+		return 9
+	def getYear(self):
+		return datetime.today().year #연도
 	
 	def parseCafeteria(self):
 		url = "http://"+self.region[self.locale]+"/sts_sci_md00_001.do?schulCode="+self.schoolcode+"&schulCrseScCode=4&schulKndScCode=04&schMmealScCode=1"	#NEIS 학교급식 정보
@@ -60,3 +66,32 @@ class tCafeteria:
 		res = res.replace("</div>", "")
 		res = res.replace("<br/>", "\n")
 		return str(res[1:len(res)-1])
+	
+	def parseSchedule(self):
+		url = "http://"+self.region[self.locale]+"/sts_sci_sf00_001.do?schulCode="+str(self.schoolcode)+"&schulCrseScCode=3&schulKndScCode=03&ay="+str(self.getYear())+"&"+"mm="+str(self.getMonth())+"&"	#학사일정 링크
+		r = requests.get(url)	#html코드를 불러온다
+		soup = BeautifulSoup(r.text, "html.parser")
+		
+		allofsche = soup.find_all("td", attrs={"class":"textL"})	#class가 textL(학사일정)인 부분만 긁어온다
+		
+		res = str(allofsche[self.findIndex()].find("span"))
+		
+		#테그 제거
+		if res == "None":
+			res = allofsche[self.findIndex()]
+			res = res.replace('''<td class="textL"''', "")
+			res = res.replace("</td>", "")
+		else:
+			res = res.replace('''<span style="color:red">''', "")
+			res = res.replace("</span>", "")
+			res = res.replace("<span>", "")
+		return res
+		
+	def findIndex(self):	#학사일정을 저장해둔 list에 index를 계산해준다
+		if self.getMonth() >= 9 or self.getMonth() <= 2:
+			if self.getMonth() >= 9:
+				return (self.getMonth() - 9) + (self.getDate() - 1) * 6
+			if self.getMonth() <= 2:
+				return (self.getMonth() + 3) + (self.getDate() - 1) * 6
+		if self.getMonth() >= 3 or self.getMonth <= 8:
+			return (self.getMonth() - 6) + (self.getDate() - 1) * 6
