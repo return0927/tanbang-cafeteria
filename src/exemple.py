d40f41e003a7bca2f5da68a5f4bcc852d6507488
w3bn00b <32405548+w3bn00b@users.noreply.github.com>
2017-10-02 00:21:31 +0900

    added new example

--- src/exemple.py
+++ src/exemple.py
@@ -2,9 +2,9 @@
 
 # Copyright (c) 2017 w3bn00b
 # See the file LICENSE for copying permission.
-# example.py v1.0
+# example.py v1.2
 # author : w3bn00b
-# description : 학교 급식 정보를 파싱해와 이를 today.txt에 저장합니다
+# description : 학교 급식 정보, 학사일정을 파싱해와 각각 meal.txt, sche.txt에 저장합니다
 # Usage : cafe = tCafeteria("학교코드", '관할지역 코드')
 # Required library : datetime, bs4, requests
 
@@ -31,7 +31,11 @@ from tCafeteria import *
 
 cafe = tCafeteria("G100000479", 'DAEJEON')	#탄방중 학생코드, 지역
 res = cafe.parseCafeteria()
+w = open("./meal.txt", "w")
+meal = w.write(res)
+w.close()
 
-w = open("./today.txt", "w")
-today = w.write(res)
+res = cafe.parseSchedule()
+w = open("./sche.txt", "w")
+sche = w.write(res)
 w.close()
